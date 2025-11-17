e8fcccaa8d39dc91f26d5cb3043056445a2e9ea3
tanulo <tanulo@barossgabor.local>
2025-11-17 14:12:09 +0100

    67

--- 7_topjoy_fetch/script.js
+++ 7_topjoy_fetch/script.js
@@ -5,7 +5,18 @@ const messageButton = document.querySelector("#messageButton");
 window.addEventListener('DOMContentLoaded', async () => {
     try {
         const uzik = await fetch("./uzik.csv")
+        //console.log(uzik)
+        uzikText = (await uzik.text()).split('\n').map(sor => `${sor.trim()}`);
+        //console.log(uzikText);fasz geci nigger
+        uzikText.forEach(uzi => {
+            messages.push(uzi)
+        });
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
+});
+
+messageButton.addEventListener("click", () => {
+    const randomMsg = messages[Math.floor(Math.random() * messages.length)];
+    messageBox.textContent = randomMsg;
 });
